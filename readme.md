219046ed2148f57b87c7875cabad59102e24ef4d
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2015-08-21 18:53:50 +0200

    init

---
+++ readme.md
@@ -0,0 +1,18 @@
+
+# moondays
+
+Get a number of moondays based on a start date and when you practice.
+
+```javascript
+
+import moondays from "moondays"
+
+const today = new Date()
+
+const days = moondays(5, today, 05, 30)
+
+days === [
+
+]
+
+```
---
+++ src/index.js
@@ -0,0 +1,54 @@
+
+
+import SunCalc from "suncalc"
+import moment  from "moment"
+
+
+export default function moondays(count, startDate = new Date, practiceHour = 1, practiceMinute = 0) {
+
+  const today = moment(startDate)
+
+  today.hour(practiceHour)
+  today.minute(practiceMinute)
+
+  let daysFound = []
+  let day = today
+
+  while(daysFound.length < count) {
+
+    let illumination = isMoonday(day)
+
+    if(isMoonday(day)) {
+      let illumination = SunCalc.getMoonIllumination(day)
+      daysFound.push({ day, illumination  })
+    }
+
+    day.add(1, "days")
+  }
+
+  return daysFound
+}
+
+
+export function isMoonday(day) {
+
+  const prevday = moment(day).subtract(1, "days")
+  const nextday = moment(day).add(1, "days")
+
+  const previllum = SunCalc.getMoonIllumination(prevday.toDate())
+  const illum = SunCalc.getMoonIllumination(day.toDate())
+  const nextillum = SunCalc.getMoonIllumination(nextday.toDate())
+
+  const ap = previllum.fraction
+  const bp = illum.fraction
+  const cp = nextillum.fraction
+
+  if(ap < bp && bp > cp) {
+    return illum
+  }
+
+  if(ap > bp && bp < cp) {
+    return illum
+  }
+
+}
